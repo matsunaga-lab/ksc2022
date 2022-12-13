2df527983ea00c92746195f58d70ede67554b4f5
Genryu Inami <genryu7@gmail.com>
2022-12-13 10:53:54 +0900

    draw points

--- public/js/index.js
+++ public/js/index.js
@@ -1,23 +1,41 @@
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera_width = window.innerWidth;
+const camera_height = window.innerHeight;
+const camera = new THREE.OrthographicCamera(camera_width / -2, camera_width / 2, camera_height / -2, camera_height / 2, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+// 形状データを作成
+const SIZE = window.innerHeight;
+// 配置する個数
+const LENGTH = 1000;
+// 頂点情報を格納する配列
+const vertices = [];
+for (let i = 0; i < LENGTH; i++) {
+	const x = SIZE * (Math.random() - 0.5);
+	const y = SIZE * (Math.random() - 0.5);
 
-camera.position.z = 5;
+	vertices.push(x, y, -1);
+}
+
+scene.add(createPointsMesh(vertices));
+
+function createPointsMesh(vertices) {
+	const geometry = new THREE.BufferGeometry();
+	geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
+
+	const material = new THREE.PointsMaterial({
+		size: 5,
+		color: 0xffffff,
+	});
+
+	return new THREE.Points(geometry, material);
+}
 
 function animate() {
 	requestAnimationFrame(animate);
-
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
-
 	renderer.render(scene, camera);
 };
 
