7e370bd52fc8e041b1cb439998ed2656b32195f5
Genryu Inami <genryu7@gmail.com>
2022-12-13 10:37:01 +0900

    install three.js

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.use(express.static('public'));
-
-app.get('/', function (req, res) {
-	res.send('Hello World')
-})
+app.use(express.static('public'))
 
 app.listen(3000)
